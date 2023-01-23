da0b2ee0918f4843326604f8ca98d92c03225727
Ivan Logutov <foreverivan2004@gmail.com>
2023-01-23 02:57:44 +0000

    adding Binary Search exercise

---
+++ Easy/704. Binary Search/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return -1;
+    }
+}
