a1cb73b08263476bf92a44cb752a3bfac3733714
Ivan Logutov <foreverivan2004@gmail.com>
2023-01-23 03:10:58 +0000

    adding Search Insert Position exercise

---
+++ Easy/35. Search Insert Position/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
