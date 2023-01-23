cff939cf806fb384411df2863b4cf18fc283befe
Ivan Logutov <foreverivan2004@gmail.com>
2023-01-23 02:52:26 +0000

    adding Remove Element exercise

---
+++ Easy/27. Remove Element/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
