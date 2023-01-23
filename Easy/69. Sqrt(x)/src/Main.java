50724e469a0d263b8a180de2c5c3b55a4dc5f5e1
Ivan Logutov <foreverivan2004@gmail.com>
2023-01-23 10:26:43 +0000

    adding Sqrt(x) exercise

---
+++ Easy/69. Sqrt(x)/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int mySqrt(int x) {
+        int result = (int) (Math.sqrt(x));
+
+        return result;
+    }
+}
