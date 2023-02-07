e01d04e0e43eecdafd070ae2a7885f07bad42682
Ivan Logutov <foreverivan2004@gmail.com>
2023-02-07 15:24:53 +0000

    adding exercise

---
+++ Easy/1071. Greatest Common Divisor of Strings/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+        int gcd = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcd);
+    }
+
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+}
