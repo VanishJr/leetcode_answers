7f419381d195cd43618b2ab0498d6376befe0b87
Ivan Logutov <foreverivan2004@gmail.com>
2023-02-07 17:06:07 +0000

    making changes

---
+++ Study Plans/Algorithm I/189. Rotate Array/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public static void rotate(int[] nums, int k) {
+        k = k % nums.length; // to handle cases where k > nums.length
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
+
